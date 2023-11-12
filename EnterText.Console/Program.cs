59c6167b0d381376b6e6975772d260eff7b7d6fc
Станислав Наравецкас <xobbit26@gmail.com>
2023-11-12 09:35:24 +0200

    123

---
+++ EnterText.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter the text:");
+EnterTheText();
+return;
+
+static void EnterTheText()
+{
+    var text = Console.ReadLine();
+    if (string.IsNullOrEmpty(text))
+    {
+        Console.WriteLine("Enter the text eblan!");
+        EnterTheText();
+    }
+    else
+    {
+        Console.WriteLine("Good text but anyway ty eblan!");
+    }
+}
